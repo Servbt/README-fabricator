a13b02bd17ae5c9e07d93ccbe39e09defc41553b
Servbt <arianrezvani00@gmail.com>
2022-07-15 00:03:19 -0400

    added some questions and imported inquirer to index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // TODO: Include packages needed for this application
 const exports = require("./generateMarkdown");
+const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What do you want the Title to be?",
+    "Write a description for what your README will say:",
+    "How will this application be installed?",
+    "Exactly how will the program be used?",
+    "Contribution Guidelines?",
+    "How to report Bugs?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
